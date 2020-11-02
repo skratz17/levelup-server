4bc28ad6504d2b5394ef93e194c965d9c1adc539
Jacob Eckert <jweckert17@gmail.com>
2020-11-02 10:12:21 -0600

    implement game ViewSet and serializer

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from levelupapi.views import register_user, login_user, GameTypes
+from levelupapi.views import register_user, login_user, GameTypes, Games
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
+router.register(r'games', Games, 'game')
 
 urlpatterns = [
     path('', include(router.urls)),
--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -1,3 +1,4 @@
 """Views Package"""
 from .auth import login_user, register_user
 from .gametype import GameTypes
+from .game import Games
