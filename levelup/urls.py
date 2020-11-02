17b9c8f385a6d20703019207b014aa1038a214fd
Jacob Eckert <jweckert17@gmail.com>
2020-11-02 15:54:17 -0600

    implement Event ViewSet and Serializer

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,11 +2,12 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from levelupapi.views import register_user, login_user, GameTypes, Games
+from levelupapi.views import register_user, login_user, GameTypes, Games, Events
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', Games, 'game')
+router.register(r'events', Events, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
